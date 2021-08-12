375db318fc263fab06d26c95befbc8bd57a9db8f
Rio Cantre <smittenaf301icu@gmail.com>
2021-08-12 20:03:14 +0200

    Installed webpack

--- src/server/index.js
+++ src/server/index.js
@@ -7,7 +7,12 @@ app.use(express.static('src/client'))
 
 console.log(__dirname)
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 
+const cors = require('cors');
+app.use(cors());
 
 // GET route
 app.get('/',  (req, res) => {
