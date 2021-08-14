7b6989f1953f7eb4c0bed5b9f934ed2e8bd614eb
Rio Cantre <smittenaf301icu@gmail.com>
2021-08-14 17:36:13 +0200

    Modified webpacks and package.json

--- webpack.prod.js
+++ webpack.prod.js
@@ -24,10 +24,6 @@ module.exports = {
                     use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
                 },
                 {
-                    test: /\.html$/i,
-                    loader: 'html-loader'
-                },
-                {
                     test: /\.(png|jpe?g|gif)$/i,
                     loader: 'file-loader',
                     options: {
@@ -41,8 +37,7 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html"
         }),
-        new MiniCssExtractPlugin({ filename: "[name].css" }),
-        new WorkboxPlugin.GenerateSW()
+        new MiniCssExtractPlugin({ filename: "[name].css" })
         
     ]
 }
