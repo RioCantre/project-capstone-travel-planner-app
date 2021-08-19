bfbc2c4dfccaf5132a0ec2259eba6dfcd2abceab
Rio Cantre <smittenaf301icu@gmail.com>
2021-08-19 19:12:30 +0200

    Added assets, form and footer files, fixed image path error

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,10 @@
-import './styles/main.scss'
+import './styles/main.scss';
+
+import img1 from './assets/img1.png';
+const img = document.querySelector('.main-img');
+img.src = img1;
+
+
+export {
+    img
+}
