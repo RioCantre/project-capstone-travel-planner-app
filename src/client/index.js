5d0e7d88e6efd78d7d3a69a18af4fb78e3dda6cf
Rio Cantre <smittenaf301icu@gmail.com>
2021-09-07 11:39:41 +0200

    Modified files, added comments

--- src/client/index.js
+++ src/client/index.js
@@ -1,22 +1,23 @@
+// functions
 import { postData, generateForm, reformatDate } from './js/app';
 import { generateEntry } from './js/formHandler';
 import { updateUI } from './js/createTrip';
 
+// main click event
 document.getElementById('generate').addEventListener('click', generateEntry);
 
-
+// style
 import './styles/main.scss';
-
 import image from './assets/img1.png';
 import image2 from './assets/img2.png';
 import image3 from './assets/img3.png';
 
-
+// assigned images value
 const img = document.querySelector('.main-img');
 const img2 = document.querySelector('.img-footer');
 const img3 = document.querySelector('.img-display');
 
-
+// images equivalent src
 img.src = image;
 img2.src = image2;
 img3.src = image3;
